7fdbff073c62be06f7337eb832f3efd494901570
41w <xusiyi@msu.edu>
2019-04-04 15:36:06 -0400

    Build a simple web server that handles any URL

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var server = http.createServer(function (request, response) {
       '<h1>Homepage</h1>' +
       '<h2>Hello World</h2>' +
       '<ul>' +
-      '<li><a href="http://samanthaxu-js-server-intro.herokuapp.com/random-joke">Jokes</a></li>' +
-      '<li><a href="http://samanthaxu-js-server-intro.herokuapp.com/cuteness">Something Cute</a></li>' +
+      '<li><a href="/random-joke">Jokes</a></li>' +
+      '<li><a href="/cuteness">Something Cute</a></li>' +
       '</ul>'
     )
   } else if (request.url === '/random-joke') {
@@ -18,21 +18,21 @@ var server = http.createServer(function (request, response) {
         '<h1>Joke #1</h1>' +
         '<h4>Knock, knock! <br> Who\'s there? <br> Double. <br>' +
         'Double who? <br> W! </h4>' +
-        '<a href="http://samanthaxu-js-server-intro.herokuapp.com/">Home</a>'
+        '<a href="/">Home</a>'
       )
     } else if (choice < 0.75) {
       response.end(
         '<h1>Joke #2</h1>' +
         '<h4>Knock, knock! <br> Who\'s there? <br> Beats. <br>' +
         'Beats who? <br> Beats me.</h4>' +
-        '<a href="http://samanthaxu-js-server-intro.herokuapp.com/">Home</a>'
+        '<a href="/">Home</a>'
       )
     } else {
       response.end(
         '<h1>Joke #3</h1>' +
         '<h4>Knock, knock! <br> Who\'s there? <br> Cook. <br>' +
         'Cook who? <br> Yeah, you do sound kinda crazy. </h4>' +
-        '<a href="http://samanthaxu-js-server-intro.herokuapp.com/">Home</a>'
+        '<a href="/">Home</a>'
       )
     }
   } else if (request.url === '/cuteness') {
@@ -40,7 +40,7 @@ var server = http.createServer(function (request, response) {
       '<h1>Something Cute Here</h1>' +
       '<a href="https://paperpull.com/best-wallpaper-of-garfield-from-the-2004-garfield-movie/">' +
       '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs9ftlUjd1dMQ3cE1WwojNO3uw2BSITFyL2mvQoWBVHsENH9yD_A" alt="Garfield">' + '</a><br>' +
-      '<a href="http://samanthaxu-js-server-intro.herokuapp.com/">Home</a>'
+      '<a href="/">Home</a>'
     )
   } else {
     response.end(
@@ -48,7 +48,7 @@ var server = http.createServer(function (request, response) {
       'The requested URL http://samanthasamanthaxu-js-server-intro.herokuapp.com' +
       request.url +
       '/ was not found on this server. That\'s all we know. <br>' +
-      '<a href="http://samanthaxu-js-server-intro.herokuapp.com/">Home</a>'
+      '<a href="/">Home</a>'
     )
   }
 })
