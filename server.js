2edcecfc370e0c575036bd65f43793f207647ac8
41w <xusiyi@msu.edu>
2019-04-04 14:31:22 -0400

    testing

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ var server = http.createServer(function (request, response) {
         '<h1>Joke #1</h1>' +
         '<h4>Knock, knock! <br> Who\'s there? <br> Double. <br>' +
         'Double who? <br> W! </h4>' +
-        '<a href="http://localhost:' + port + '/">Home</a>'
+        '<a href="http://samanthaxu-js-server-intro.herokuapp.com/">Home</a>'
       )
     } else if (choice < 0.75) {
       response.end(
         '<h1>Joke #2</h1>' +
         '<h4>Knock, knock! <br> Who\'s there? <br> Beats. <br>' +
         'Beats who? <br> Beats me.</h4>' +
-        '<a href="http://localhost:' + port + '/">Home</a>'
+        '<a href="http://samanthaxu-js-server-intro.herokuapp.com/">Home</a>'
       )
     } else {
       response.end(
         '<h1>Joke #3</h1>' +
         '<h4>Knock, knock! <br> Who\'s there? <br> Cook. <br>' +
         'Cook who? <br> Yeah, you do sound kinda crazy. </h4>' +
-        '<a href="http://localhost:' + port + '/">Home</a>'
+        '<a href="http://samanthaxu-js-server-intro.herokuapp.com/">Home</a>'
       )
     }
   } else if (request.url === '/cuteness') {
@@ -36,12 +36,12 @@ var server = http.createServer(function (request, response) {
       '<h1>Something Cute Here</h1>' +
       '<a href="https://paperpull.com/best-wallpaper-of-garfield-from-the-2004-garfield-movie/">' +
       '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs9ftlUjd1dMQ3cE1WwojNO3uw2BSITFyL2mvQoWBVHsENH9yD_A" alt="Garfield">' + '</a><br>' +
-      '<a href="http://localhost:' + port + '/">Home</a>'
+      '<a href="http://samanthaxu-js-server-intro.herokuapp.com/">Home</a>'
     )
   } else {
     response.end(
       '<h1>Page Not Found</h1>' +
-      'The requested URL http://' +
+      'The requested URL http://samanthasamanthaxu-js-server-intro.herokuapp.com' +
       request.url +
       '/ was not found on this server. That\'s all we know.'
     )
